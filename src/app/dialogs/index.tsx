d1388f4fd5b2078b0a52980cb1ea3c174803eb76
Dương <duong_dinh@datahouse.com>
2025-05-25 18:35:20 +0700

    fix: build

--- src/app/dialogs/index.tsx
+++ src/app/dialogs/index.tsx
@@ -1,9 +1,9 @@
 import NiceModal from "@ebay/nice-modal-react";
 import React from "react";
 
-const ProductFilter = React.lazy(() => import("../dialogs/ProductFilter"));
-const SideBar = React.lazy(() => import("../dialogs/Sidebar"));
-const MenuProduct = React.lazy(() => import("../dialogs/MenuProduct"));
+const ProductFilter = React.lazy(() => import("./ProductFilter"));
+const SideBar = React.lazy(() => import("./Sidebar"));
+const MenuProduct = React.lazy(() => import("./MenuProduct"));
 
 export const registerDialogs = () => {
   NiceModal.register("product-filter", ProductFilter);
